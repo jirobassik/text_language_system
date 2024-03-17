b2c069aaa6d3577a91aa32d9b9911dd520259ca5
= <jirobassik@tut.by>
2024-03-17 12:27:31 +0300

    Fix apikey model

--- api_key/models.py
+++ api_key/models.py
@@ -7,12 +7,14 @@ from django.utils import timezone
 def generate_api_key(key_len=32):
     return token_hex(key_len)
 
+def default_expired_at():
+    return timezone.now() + timezone.timedelta(weeks=1)
 
 class ApiKeyModel(models.Model):
     api_token = models.CharField('API ключ', default=generate_api_key, editable=False, unique=True)
     created_at = models.DateTimeField('Время добавления', auto_created=True, editable=False, auto_now_add=True)
     expired_at = models.DateTimeField('Время истечения', editable=True, auto_created=True,
-                                      default=timezone.now() + timezone.timedelta(weeks=1))
+                                      default=default_expired_at)
     is_deleted = models.BooleanField(verbose_name='Удалено', default=False)
     is_expired = models.BooleanField(verbose_name='Истек', default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
