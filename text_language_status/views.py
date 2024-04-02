5a06b444a442313db7e294c7f9123453438d4ade
= <jirobassik@tut.by>
2024-04-02 09:30:49 +0300

    Add auto update for status

--- text_language_status/views.py
+++ text_language_status/views.py
@@ -11,3 +11,8 @@ class StatusListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         self.queryset = self.model.objects.filter(user=self.request.user, is_deleted=False)
         return super().get_queryset()
+
+    def get(self, request, *args, **kwargs):
+        if self.request.htmx:
+            self.template_name = "text_language_status/status_table.html"
+        return super().get(request)
